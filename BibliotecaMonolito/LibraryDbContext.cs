5791099e8570fa7c487edee9c4a561cc0adc56c2
lforellana <reckoner1991@gmail.com>
2026-08-04 20:45:15 -0600

    Agregando Migraciones

---
+++ BibliotecaMonolito/LibraryDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft EntityFrameworkCore;
+using BibliotecaMonolito.Models;
+
+namespace BibliotecaMonolito;
+
+public class LibraryDbContext; DbContext
+
+
+public LibrarDbContext(DbContetOption)
+public Dbset<Autor>
